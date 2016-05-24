b2012b20e3aa55b6f7c8d94b1be7be8a65fc7d4f
gwenf <gwenfaraday@gmail.com>
2016-05-24 17:51:35 -0400

    styled navbar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,13 +2,41 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
+
+  onSearch: function(e){
+    alert('not yet wired up')
+  },
+
   render: function(){
     return (
-    <div>
-      <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to='/examples' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-    </div>
+      <div className='top-bar'>
+        <div className='top-bar-left'>
+          <ul className='menu'>
+            <li className='menu-text'>Local Weather App</li>
+            <li>
+              <IndexLink to='/' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to='/about' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to='/examples' activeClassName='active' activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className='top-bar-right'>
+          <form onSubmit={this.onSearch}>
+            <ul className='menu'>
+              <li>
+                <input type='search' placeholder='Search Weather...' />
+              </li>
+              <li>
+                <input type='submit' className='button' value='Get Weather' />
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
     )
   }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+var webpack = require('webpack');
+
 module.exports = {
   // devtool: 'inline-source-map',
   devtool: 'cheap-module-eval-source-map',
-  entry: './app/app.jsx',
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js',
